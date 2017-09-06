86eaff9614e0d992a19aeb6d135aba287b7f2c40
kgreen14 <kgreen14@gmail.com>
2017-09-06 11:57:25 -0400

    show route created and the show view for creature

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -16,11 +16,23 @@ class CreaturesController < ApplicationController
     # create a new creature from `creature_params`
     creature = Creature.new(creature_params)
 
-    # if creature saves, redirect to route that displays all creatures
-    if creature.save
-      redirect_to creatures_path
-      # redirect_to creatures_path is equivalent to:
-      # redirect_to "/creatures"
+        # if creature saves, redirect to route that displays all creatures
+        if creature.save
+        redirect_to creature_path(creature)
+        # redirect_to creatures_path is equivalent to:
+        # redirect_to "/creatures"
+        end
     end
+
+    def show
+    # get the creature id from the url params
+    creature_id = params[:id]
+
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    @creature = Creature.find_by_id(creature_id)
+
+    # render the show view (it has access to instance variable)
+    # remember the default behavior is to render :show
   end
 end
